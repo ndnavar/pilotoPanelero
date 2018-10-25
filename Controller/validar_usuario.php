b7cacf454c10afe568569dd7d6a09e566ea881bc
ndnavar <44449934+ndnavar@users.noreply.github.com>
2018-10-25 16:19:18 -0500

    Update validar_usuario.php

--- Controller/validar_usuario.php
+++ Controller/validar_usuario.php
@@ -1,10 +1,14 @@
+
 <?php
+//llama la función de inicio de sesion que requiere los datos de los usuarios
 session_start();
+//Este programa es el encargado de hacer la validación de usuarios en en las bases de datos,esto tomando los datos ingresados y comparandolos con los existentes
 //datos para establecer la conexion con la base de mysql.
 require('../View/common.php');
 
 function quitar($mensaje)
 {
+//Caraxteres que no deben existir en los usuarios
 	$nopermitidos = array("'",'\\','<','>',"\"");
 	$mensaje = str_replace($nopermitidos, "", $mensaje);
 	return $mensaje;
@@ -20,8 +24,9 @@ if($_POST["username"] != "" && $_POST["password"] != "")
 	
 	$result = $db->db->query($query);
 	
-	
+	//comparacion con la base de datos
 	if($row = $result->fetch_array() ){
+		//Validacion correcta
 		if($row["U_password"] == $password){
 			setcookie('k_username', $username , time()+ (10 * 365 * 24 * 60 * 60));
 			$_SESSION["k_id"] = $row['id'];
@@ -31,12 +36,14 @@ if($_POST["username"] != "" && $_POST["password"] != "")
 			
 			echo 'ingreso exitoso';
 		}else{
+			// ingeso no valido
 			die("Password incorrecto".$password);
 		}
 	//}else{
 	//	die("Usuario no registrado");
 	//	}
 	}else{
+		//si no encuentra
 		die("Username no existente en la base de datos");
 	}
 	$result->free_result();
