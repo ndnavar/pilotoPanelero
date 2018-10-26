6ca350f9ba16cbd6c17ef41f79086a25dad27854
ndnavar <44449934+ndnavar@users.noreply.github.com>
2018-10-26 10:59:26 -0500

    Update add_control_point.php

--- Controller/add_control_point.php
+++ Controller/add_control_point.php
@@ -1,6 +1,9 @@
+
 <?php
+//Esta sección esta desarrollada para hacer la creación de un nuevo punto de control.
 require('../View/common.php');
-if($_POST){	
+if($_POST){
+	// Se accede a la base de datos y se agrega el codigo del producto y la ubicación en la que esta 
 	$result = $db->add_control_point($_POST['codigo'],$_POST['lat'],$_POST['lon'],$_POST['registrador']);
 	
 	echo json_encode($result);
